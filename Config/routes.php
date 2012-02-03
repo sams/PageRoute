fe1cf980edc3dde3245cc864053b639aa806756a
unknown <sam.sherlock@gmail.com>
2012-02-03 18:13:19 +0000

    if connectHome set / route to pages display home

--- Config/routes.php
+++ Config/routes.php
@@ -4,3 +4,6 @@ App::uses('PageRoute', 'PageRoute.Routing/Route');
 Router::connect('/:page', array('controller' => 'pages', 'action' => 'display'),
 	array('routeClass' => 'PageRoute')
 );
+if(Configure::read('PageRoute.connectHome')) {
+  Router::connect('/', array('controller' => 'pages', 'action' => 'display', 'home'));
+}
